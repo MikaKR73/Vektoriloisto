a99db0a6588f85579253cff1131af9769405a7dd
Mika Rasinmäki <mika.rasinmaki@ictpro.labra>
2020-11-19 13:19:57 +0200

    Lisätty 2 ledi. Korjailtu virheitä

--- Vektoriloisto.ino
+++ Vektoriloisto.ino
@@ -2,10 +2,10 @@
 #define LedS2V 11 // Määritellään ledin paikka
 #define maksimiS1 255 // Maksimi tehoS1 ledille
 #define maksimiS2 250 // Maksimi tehoS1 ledille
-#define minimiS1 0
-#define minimiS2 10
-#define lisaaS1 1
-#define lisaaS2 2
+#define minimiS1 0 // Määrittää Minimin
+#define minimiS2 10 // Määrittää Minimin
+#define lisaaS1 1 //Määrittää laskenta nopeuden
+#define lisaaS2 2 //Määrittää laskenta nopeuden
 
 void setup(){
   pinMode(LedS1V, OUTPUT);
@@ -16,9 +16,8 @@ int tehoS1 = 0, tehoS2 = 0; // Märittää tehoS1n nollaan
 bool laskentaS1 = true, laskentaS2 = true; // Merkitsee pienenetäänkö ledin tehoS1ja
 
 void BreathS1(){
-  analogWrite(LedS1V, tehoS1); // Kirjotaa ledille tehoS1n
-    int x = Serial.read();
- if(laskentaS1 == true){
+ analogWrite(LedS1V, tehoS1); // Kirjotaa ledille tehoS1
+    if(laskentaS1 == true){
   tehoS1 = tehoS1 + lisaaS1; //Kasvattaa muuttujaa yhdellä ja tulostaa tiedot
     Serial.print("Menee ylös S1: ");
     Serial.println(tehoS1);
@@ -40,9 +39,8 @@ void BreathS1(){
 }
 
 void BreathS2(){
-  analogWrite(LedS2V, tehoS2); // Kirjotaa ledille tehoS1n
-    int x = Serial.read();
- if(laskentaS2 == true){
+ analogWrite(LedS2V, tehoS2); // Kirjotaa ledille tehoS2
+    if(laskentaS2 == true){
   tehoS2 = tehoS2 + lisaaS2; //Kasvattaa muuttujaa yhdellä ja tulostaa tiedot
     Serial.print("Menee ylös S2: ");
     Serial.println(tehoS2);
@@ -51,7 +49,7 @@ void BreathS2(){
    Serial.print("Menee alas S2: ");
    Serial.println(tehoS2);
  }
- if(tehoS1 >= maksimiS2){
+ if(tehoS2 >= maksimiS2){
    laskentaS2 = false; // merkitsee muuttujan pienetämiseen kun saatu maksimi arvo
    Serial.print("Saavutettu maksimi S2: ");
    Serial.println(tehoS2);

95887dd5c36fc4745a5e7ca6c7a5671c8d507ce9
Mika Rasinmäki <mika.rasinmaki@ictpro.labra>
2020-11-19 11:07:55 +0200

    Laitettu ledi kirkastumaan ja himenemään

--- Vektoriloisto.ino
+++ Vektoriloisto.ino
@@ -1,22 +1,28 @@
-#define LedS1V 10
-#define maksimi 255
+#define LedS1V 10 // Määritellään ledin paikka
+#define maksimi 255 // Maksimi teho ledille
 
 void setup(){
   pinMode(LedS1V, OUTPUT);
     Serial.begin(9600);
 }
-#define VIIVE 50
-int teho=0;
+#define VIIVE 50 // Määritetään viive
+int teho = 0;
+bool laskenta = true; //merkitsee pienenetäänkö ledin tehoja
 
 void loop(){
   analogWrite(LedS1V, teho);
     int x = Serial.read();
     Serial.println(teho);
- if(teho < maksimi){
+ if(laskenta == true){
   teho++; //Kasvattaa muuttujaa yhdellä
+ }else{
+   teho--; //Pienetää muutujaa
  }
-  if(teho >= maksimi){
-    teho = 0; //Nolaa muuttujan
-  }
-  delay(VIIVE);
+ if(teho >= maksimi){
+   laskenta = false;
+ }
+ if(teho <=0){
+  laskenta = true;
+ }
+   delay(VIIVE);
 }

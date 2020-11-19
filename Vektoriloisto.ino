80288bd945afff909e4003803aedca7bc4930fc5
Mika Rasinmäki <mika.rasinmaki@ictpro.labra>
2020-11-19 10:28:20 +0200

    Analooginen käyttö, vilkkuu ja serial ilmoitukset

--- Vektoriloisto.ino
+++ Vektoriloisto.ino
@@ -1,18 +1,22 @@
 #define LedS1V 10
+#define maksimi 255
 
 void setup(){
   pinMode(LedS1V, OUTPUT);
+    Serial.begin(9600);
 }
-#define VIIVE 250
+#define VIIVE 50
+int teho=0;
+
 void loop(){
-  analogWrite(LedS1V, 0);
+  analogWrite(LedS1V, teho);
+    int x = Serial.read();
+    Serial.println(teho);
+ if(teho < maksimi){
+  teho++; //Kasvattaa muuttujaa yhdellä
+ }
+  if(teho >= maksimi){
+    teho = 0; //Nolaa muuttujan
+  }
   delay(VIIVE);
-  analogWrite(LedS1V, 64);
-  delay(VIIVE);
-  analogWrite(LedS1V, 127);
-  delay(VIIVE);
-  analogWrite(LedS1V, 191);
-  delay(VIIVE);
-  analogWrite(LedS1V, 255);
-  delay(VIIVE * 5);
 }

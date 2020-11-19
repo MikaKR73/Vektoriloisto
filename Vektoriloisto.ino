92d165e26b85b6b6cb60278e82c88b77f7a6c22b
Mika Rasinmäki <mika.rasinmaki@ictpro.labra>
2020-11-19 10:01:50 +0200

    Analooginen käyttö ja vilkkuu

---
+++ Vektoriloisto.ino
@@ -0,0 +1,18 @@
+#define LedS1V 10
+
+void setup(){
+  pinMode(LedS1V, OUTPUT);
+}
+#define VIIVE 250
+void loop(){
+  analogWrite(LedS1V, 0);
+  delay(VIIVE);
+  analogWrite(LedS1V, 64);
+  delay(VIIVE);
+  analogWrite(LedS1V, 127);
+  delay(VIIVE);
+  analogWrite(LedS1V, 191);
+  delay(VIIVE);
+  analogWrite(LedS1V, 255);
+  delay(VIIVE * 5);
+}
